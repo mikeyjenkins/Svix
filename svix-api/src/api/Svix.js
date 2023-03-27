51abdae0261c7c831e1684f4a9c6054e2db94f09
Michael Jenkins <mikeyjenkins9@gmail.com>
2023-03-27 17:32:21 -0600

    Update Svix.js

--- svix-api/src/api/Svix.js
+++ svix-api/src/api/Svix.js
@@ -2,4 +2,4 @@ import { Svix } from "svix";
 
 
 //create connection with Svix client
-export const svix = new Svix("testsk_VKmUKgABTY1L3ejvKKkl2rKSZwaS-LCC.eu");
+export const svix = new Svix("API_KEY_HERE");
