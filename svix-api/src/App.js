d75f96819b73950e726004575478964819d0c9db
Michael Jenkins <mikeyjenkins9@gmail.com>
2023-03-27 16:20:17 -0600

    edit event modal

--- svix-api/src/App.js
+++ svix-api/src/App.js
@@ -5,10 +5,15 @@ import * as eventAPI from "./api/EventsAPI";
 import Button from "react-bootstrap/Button";
 import AddIcon from "@mui/icons-material/Add";
 import CreateEventModal from "./components/CreateEventModal";
+import ListGroup from "react-bootstrap/ListGroup";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import EditModal from "./components/EditEventModal";
 
 function App() {
   const [showCreateModal, setShowCreateModal] = useState(false);
+  const [showEditModal, setShowEditModal] = useState(false);
   const [events, setEvents] = useState([]);
+  const [currentEvent, setCurrentEvent] = useState();
 
   const createEvent = (event) => {
     eventAPI.createEvent(event).then(() => {});
@@ -18,28 +23,54 @@ function App() {
     eventAPI.listEvents().then((res) => {
       //update events state to match result from API call
       setEvents(res);
-    })
-  }
-  
+    });
+  };
+
   //get list of events when page renders
   useEffect(() => {
     listEvents();
-  }, [])
+  }, []);
 
   return (
-    <div className="App">
-      <Button
-        id="primary_btn"
-        size="sm"
-        onClick={() => setShowCreateModal(true)}
-      >
-        <AddIcon />
-      </Button>{" "}
-      <CreateEventModal
-        show={showCreateModal}
-        setShowCreateModal={setShowCreateModal}
-        createEvent={createEvent}
-      />
+    <div className="events_page_wrapper">
+      <div className="events_component">
+        <div className="events_header">
+          <h3>Events</h3>
+          <Button
+            id="primary_btn"
+            size="sm"
+            onClick={() => setShowCreateModal(true)}
+          >
+            <AddIcon />
+          </Button>{" "}
+        </div>
+        <div className="events_list">
+          <ListGroup>
+            {events.map((event, index) => (
+              <ListGroup.Item
+                key={index}
+                onClick={() => {
+                  setCurrentEvent(event);
+                  setShowEditModal(true);
+                }}
+                className="list_item"
+              >
+                {event.name} <ArrowForwardIosIcon />
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        </div>
+        <CreateEventModal
+          show={showCreateModal}
+          setShowCreateModal={setShowCreateModal}
+          createEvent={createEvent}
+        />
+        {showEditModal && <EditModal
+          event={currentEvent}
+          show={showEditModal}
+          setShowEditModal={setShowEditModal}
+        />}
+      </div>
     </div>
   );
 }
