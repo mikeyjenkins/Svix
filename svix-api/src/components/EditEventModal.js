48d92277638c91a196f58b30190a67cb5fa63d08
Michael Jenkins <mikeyjenkins9@gmail.com>
2023-03-27 17:12:35 -0600

    primary button cleanup

--- svix-api/src/components/EditEventModal.js
+++ svix-api/src/components/EditEventModal.js
@@ -2,7 +2,6 @@ import {React, useState} from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form'
-import '../styles/events.scss';
 
 
 
